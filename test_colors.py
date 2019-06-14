61cfb049ed5862afcc5acb0788ec72cb05e0fdfc
ViniciusBruce <viniciusxnicolas007@gmail.com>
2019-06-13 22:16:09 -0300

    creating test_colors.py but is not running

---
+++ test_colors.py
@@ -0,0 +1,8 @@
+def colorCode(code):
+    print('\033[{}:{}:{}m')
+def colorsTest():
+    numbersRange = (0, 6)
+    for foregroundNumber in range(numbersRange):
+        for backgroundNumber in range(numbersRange):
+            
+
