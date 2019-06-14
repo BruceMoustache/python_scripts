16aa5c740a13c94814eef5ffb838299723631d7f
ViniciusBruce <viniciusxnicolas007@gmail.com>
2019-06-13 21:07:23 -0300

    creating randomer.py with screen and menu but without functinoally

---
+++ randomer.py
@@ -0,0 +1,24 @@
+from random import *
+
+def menu():
+    menuString = (
+"""
+[ 1 ] numero aleatorio
+[ 2 ] lista aleatoria
+[ 0 ] sair
+
+--> """)
+    choice = int(input(menuString))
+    print(choice)
+
+def sair():
+    exit()
+
+def numero():
+    pass
+
+def lista()
+    pass
+
+menu()
+
