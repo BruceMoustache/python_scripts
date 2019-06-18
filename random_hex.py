0b400dc37009f868207d900151bfd626fa09a015
ViniciusBruce <viniciusxnicolas007@gmail.com>
2019-06-18 20:03:42 -0300

    add functionallity to random_hex.py

--- random_hex.py
+++ random_hex.py
@@ -1,9 +1,22 @@
+from random import randint
+
 def hexToDec(hexNumber):
     pass
 
 def decToHex(decNumber):
     pass
 
-n = int(input())
-print(decToHex(n))
+def randomHexInInterval(lower, higher):
+    number = randint(lower, higher)
+    hexNumber = hex(number)[2:].upper()
+    return hexNumber
+
+def randomHexWithDigits(digitsCount):
+    lower = 16 ** digitsCount
+    higher = 16 ** (digitsCount + 1) - 1
+    return randomHexInInterval(lower, higher)
+
+if __name__ == '__main__':
+    digitsCount = int(input('Digits count: '))
+    print( randomHexWithDigits(digitsCount) )
 
